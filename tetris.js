7142f3e364f5f1d368c784b0c4e0dd1e3a78c9a4
syh80881 <syh8088@gmail.com>
2020-04-19 21:17:55 +0900

    tetris
    tetris

--- tetris.js
+++ tetris.js
@@ -1,7 +1,20 @@
+Object.prototype.isEmpty = function() {
+    for(var key in this) {
+        if(this.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+};
+
+
+
 var tetrisDom = document.querySelector("#tetris");
+var nextTetrisDom = document.querySelector("#next-table");
 var tetris = [];
-var targetShape = [];
 var targetBlock = {};
+var targetShape = [];
+var nextTargetBlock = {};
+var nextTargetShape = [];
 var exitCoordinates = { x: 0, y: 3 };
 var blocks = [
     {
@@ -189,11 +202,45 @@ var blocks = [
 
 
 initialize();
-let int = setInterval(oneSpaceDown, 2000);
+let int = setInterval(oneSpaceDown, 500);
 
 function initialize() {
     initWrapFrame();
+    blockWarmUp();
     blockGenerate();
+}
+
+function blockFactory() {
+    var targetBlock = JSON.parse(JSON.stringify(blocks[Math.floor(Math.random() * blocks.length)]));
+    var targetShape = JSON.parse(JSON.stringify(targetBlock['shape'][Math.floor(Math.random() * targetBlock['shape'].length)]));
+    return { targetBlock: targetBlock, targetShape: targetShape }
+}
+
+function blockWarmUp() {
+
+      if(!nextTargetBlock.isEmpty() && nextTargetShape.length  > 0) { // 그 다음시
+          targetBlock = JSON.parse(JSON.stringify(nextTargetBlock));
+          targetShape = JSON.parse(JSON.stringify(nextTargetShape));
+
+          blockGenerate();
+      } else { // 초기시
+          var target = blockFactory();
+          targetBlock = target['targetBlock'];
+          targetShape = target['targetShape'];
+      }
+
+  //  if(nextTargetBlock.length > 0 || nextTargetShape.isEmpty()) {
+        var nextTarget = blockFactory();
+        nextTargetBlock = nextTarget['targetBlock'];
+        nextTargetShape = nextTarget['targetShape'];
+        nextBlockGenerate();
+
+ //   }
+
+
+
+/*    targetBlock = (nextTargetBlock.length > 0) ? nextTargetBlock : JSON.parse(JSON.stringify(blocks[Math.floor(Math.random() * blocks.length)]));
+    targetShape = (nextTargetShape.isEmpty()) ? nextTargetShape : JSON.parse(JSON.stringify(targetBlock['shape'][Math.floor(Math.random() * targetBlock['shape'].length)]));*/
 }
 
 function initWrapFrame() {
@@ -218,11 +265,23 @@ function initWrapFrame() {
     console.log("tetrisDom", tetrisDom)
 }
 
+function nextBlockGenerate() {
+    var coordinateInjectedBlock = coordinateInjection(nextTargetShape, targetBlock['color'], exitCoordinates['x'], exitCoordinates['y']);
+
+    (coordinateInjectedBlock || []).forEach((col, i) => {
+        col.forEach((row, j) => {
+            nextTargetShape[i].splice(row['y'], 1, { ...roomInject((row['value'] === 0) ? 'black' : row['color'], row['value']), ...{ control: true } });
+        });
+    });
+
+    nextDraw();
+}
+
 function blockGenerate() {
    // tetrisRemove();
 
-    targetBlock = blocks[2];
-    targetShape = targetBlock['shape'][0];
+   // targetBlock = blocks[2];
+   // targetShape = targetBlock['shape'][0];
     console.log("targetBlock", targetBlock)
     console.log("targetShape", targetShape)
 
@@ -241,8 +300,8 @@ function blockGenerate() {
 
 }
 
-function coordinateInjection(block, color, x, y) {
-    var coordinateInjectedBlock = (block || []).map((col, i) => {
+function coordinateInjection(shape, color, x, y) {
+    var coordinateInjectedBlock = (shape || []).map((col, i) => {
         return col.map((row, j) => {
             var coordinate = { x: i + x, y: j + y };
             return { ...coordinate, ...roomInject((row) ? color : 'black', row) };
@@ -259,6 +318,32 @@ function tetrisRemove() {
     for(let i = 0;  i < len; i++) {
         tetrisDom.children[0].remove();
     }
+}
+
+function nextTetrisRemove() {
+    var len = nextTetrisDom.children.length;
+    for(let i = 0;  i < len; i++) {
+        nextTetrisDom.children[0].remove();
+    }
+}
+
+function nextDraw() {
+    nextTetrisRemove();
+    let fragment = document.createDocumentFragment();
+    nextTargetShape.forEach((col, i) => {
+
+        let tr = document.createElement('tr');
+        fragment.appendChild(tr);
+        col.forEach((row, j) => {
+            let td = document.createElement('td');
+            td.className = (row['value']) ? row.color : 'black';
+
+            tr.appendChild(td);
+        });
+    });
+
+    console.log("??fragment", fragment)
+    nextTetrisDom.appendChild(fragment);
 }
 
 function draw() {
@@ -284,17 +369,49 @@ function oneSpaceDown() {
     console.log(" oneSpaceDown ")
     var cloneTetris = JSON.parse(JSON.stringify(tetris));
     var reverseCloneTetris = cloneTetris.reverse();
-    console.log("reverseCloneTetris", reverseCloneTetris)
 
-    tetris.forEach((col, i) => {
 
-   //     if()
+    let i = 0;
+    let reverseCloneTetrisColLen = reverseCloneTetris.length;
+    for(; i < reverseCloneTetrisColLen ; i++) {
+        if(checkTheBottom(reverseCloneTetris[i]) && i === 0) {
+            console.log("밑바닥 확인");
 
-        //tetris[i]
-        col.forEach((row, j) => {
-            if(row['control']) {
-              //  tetris[i + 1];
+            removeControllerInitData(reverseCloneTetris);
+            tetris = reverseCloneTetris.reverse();
+            clearInterval(int);
+            blockWarmUp();
+            break;
+        }
+
+        let j = 0;
+        let reverseCloneTetrisRowLen = reverseCloneTetris[i].length;
+        for (; j < reverseCloneTetrisRowLen; j++) {
+            if (reverseCloneTetris[i][j]['control']) {
+                if (i - 1 >= 0) {
+                    reverseCloneTetris[i - 1].splice(j, 1, reverseCloneTetris[i][j]);
+                    reverseCloneTetris[i].splice(j, 1, {...roomInject("black", 0)});
+                }
             }
+        }
+    }
+
+    console.log("reverseCloneTetris", reverseCloneTetris)
+    tetris = reverseCloneTetris.reverse();
+
+    draw();
+}
+
+function checkTheBottom(checkArray) {
+    return (checkArray || []).some((data) => {
+        return data.value === 1
+    })
+}
+
+function removeControllerInitData(reverseCloneTetris) {
+    (reverseCloneTetris || []).forEach((col, i) => {
+        col.forEach((row, j) => {
+            reverseCloneTetris[i][j] = { ...roomInject(row.color, row.value) };
         });
     });
 }
